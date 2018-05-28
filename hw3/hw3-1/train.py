4c7971e4e92fb7e4e59fc6951c8a4691a842012c
bingbingwei <da20jeja1996@gmail.com>
2018-05-28 19:56:07 +0800

    gradient penalty

--- hw3/hw3-1/train.py
+++ hw3/hw3-1/train.py
@@ -3,6 +3,7 @@ from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 from torchvision.transforms import ToTensor
 from torch.autograd import Variable
+import torch.autograd as autograd
 import numpy as np
 import argparse
 import cv2
@@ -43,14 +44,31 @@ def dcgan_train(data_loader, generator, discriminator, optimizer_G, optimizer_D,
             print('d_loss = {}'.format(d_loss.data))
             d_loss.backward()
             optimizer_D.step()
-            im_fake_output = np.clip(im_fake.data.numpy(), 0, 1)
+            im_fake_output = np.clip(im_fake.cpu().data.numpy(), 0, 1)
             if i % 40  == 0:
                 im_fake_output = np.round(im_fake_output[0]*255)
                 im_fake_output = np.transpose(im_fake_output , (1, 2, 0))
                 print(im_fake_output.shape)
                 cv2.imwrite('iter{}.jpg'.format(i), im_fake_output)
 
-def wgan_train(data_loader, generator, discriminator, optimizer_G, optimizer_D, epoch, clip_val=0.01):
+def compute_gradient_penalty(D, real_samples, fake_samples):
+    """Calculates the gradient penalty loss for WGAN GP"""
+    # Random weight term for interpolation between real and fake samples
+    Tensor = torch.FloatTensor
+
+    alpha = Tensor(np.random.random((real_samples.size(0), 1, 1, 1)))
+    # Get random interpolation between real and fake samples
+    interpolates = (alpha * real_samples + ((1 - alpha) * fake_samples)).requires_grad_(True)
+    d_interpolates = D(interpolates)
+    fake = Variable(Tensor(real_samples.shape[0], 1).fill_(1.0), requires_grad=False)
+    # Get gradient w.r.t. interpolates
+    gradients = autograd.grad(outputs=d_interpolates, inputs=interpolates,
+                              grad_outputs=fake, create_graph=True, retain_graph=True,
+                              only_inputs=True)[0]
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
+
+def wgan_train(data_loader, generator, discriminator, optimizer_G, optimizer_D, epoch, clip_val=0.01, LAMBDA=10, use_GP=False):
     Tensor = torch.FloatTensor
     for e in range(epoch):
         desc = 'Epoch {}'.format(e)
@@ -63,22 +81,23 @@ def wgan_train(data_loader, generator, discriminator, optimizer_G, optimizer_D, 
             noise = to_var(Tensor(np.random.normal(0, 1, (batch_size, opts.latent_size))))
             im_fake = generator(noise)
             g_loss = -torch.mean(discriminator(im_fake))
-            print('g_loss: {}'.format(g_loss))
             g_loss.backward()
             optimizer_G.step()
-
-            d_loss = -torch.mean(discriminator(im_valid)) + torch.mean(discriminator(im_fake.detach()))
-            print('d_loss: {}'.format(d_loss))
+            if use_GP:
+                gradient_penalty = compute_gradient_penalty(discriminator, im_valid.data, im_fake.data)
+                d_loss = -torch.mean(discriminator(im_valid)) + torch.mean(discriminator(im_fake.detach())) + LAMBDA * gradient_penalty
+            else:
+                d_loss = -torch.mean(discriminator(im_valid)) + torch.mean(discriminator(im_fake.detach()))
             d_loss.backward()
             optimizer_D.step()
             #Weight Clipping
-            for param in discriminator.parameters():
-                param.data.clamp_(-clip_val, clip_val)
-            im_fake_output = np.clip(im_fake.data.numpy(), 0, 1)
+            if not use_GP:
+                for param in discriminator.parameters():
+                    param.data.clamp_(-clip_val, clip_val)
+            im_fake_output = np.clip(im_fake.cpu().data.numpy(), 0, 1)
             if i % 40  == 0:
                 im_fake_output = np.round(im_fake_output[0]*255)
                 im_fake_output = np.transpose(im_fake_output , (1, 2, 0))
-                print(im_fake_output.shape)
                 cv2.imwrite('iter{}.jpg'.format(i), im_fake_output)
 
 def main(opts):
@@ -100,9 +119,9 @@ def main(opts):
     optimizer_G = torch.optim.Adam(generator.parameters(), lr= 0.0002)
     optimizer_D = torch.optim.Adam(discriminator.parameters(), lr= 0.0002)
 
-    if opts.use_wgan:
+    if opts.use_wgan or opts.use_wgan_gp:
         print('WGAN Training')
-        wgan_train(data_loader,generator,discriminator,optimizer_G,optimizer_D, opts.epoch)
+        wgan_train(data_loader,generator,discriminator,optimizer_G,optimizer_D, opts.epoch, use_GP=opts.use_wgan_gp)
     else:
         print('DCGAN Training')
         dcgan_train(data_loader,generator,discriminator,optimizer_G,optimizer_D, criterion_dcgan, opts.epoch)
@@ -115,5 +134,6 @@ if __name__ == '__main__':
     parser.add_argument('-l', type=int, default= 100, dest='latent_size', help='Latent Number')
     parser.add_argument('-e', type=int, default= 1, dest='epoch', help='Epoch Number')
     parser.add_argument('-WGAN', action= 'store_true', default= False, dest= 'use_wgan', help='use WGAN Weight Clipping')
+    parser.add_argument('-WGAN-GP', action= 'store_true', default= False, dest= 'use_wgan_gp', help='use WGAN Weight Clipping')
     opts = parser.parse_args()
     main(opts)
